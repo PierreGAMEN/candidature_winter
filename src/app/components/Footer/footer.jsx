71be3f40fd1768c333a72be03922dcfa7b8fa394
Pierre GAMEN <pierre.gamen@gmail.com>
2024-06-07 17:31:12 +0200

    add: component footer + style

---
+++ src/app/components/Footer/footer.jsx
@@ -0,0 +1,19 @@
+import styles from "./footer.scss"
+
+const Footer = () => {
+    return(
+    <div className="footer">
+        <img 
+        src="https://assets-global.website-files.com/64e0b797e5ca48456ffda312/64e53e34eab4da497a5a78e1_Groupe%201083.svg" 
+        alt="Flamme et décoration" />
+        <div>
+        <p>Site original : Winter Energie</p>
+        <p>Réinterpreté par : Pierre GAMEN</p>
+        </div>
+        <img 
+        src="https://assets-global.website-files.com/64e0b797e5ca48456ffda312/64e53e34eab4da497a5a78e1_Groupe%201083.svg" 
+        alt="Flamme et décoration" />
+    </div>)
+}
+
+export default Footer;
