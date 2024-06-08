bbbd41ad2ea1aa434727b1331a6881503849aa97
PierreGAMEN <pierre.gamen@gmail.com>
2024-06-08 10:26:08 +0200

    add(footer) : text and modify font size

--- src/app/components/Footer/footer.jsx
+++ src/app/components/Footer/footer.jsx
@@ -9,6 +9,7 @@ const Footer = () => {
         <div>
         <p>Site original : Winter Energie</p>
         <p>Réinterpreté par : Pierre GAMEN</p>
+        <p>Site réalisé avec le FrameWork Next.js et déployé avec Vercel</p>
         </div>
         <img 
         src="https://assets-global.website-files.com/64e0b797e5ca48456ffda312/64e53e34eab4da497a5a78e1_Groupe%201083.svg" 
