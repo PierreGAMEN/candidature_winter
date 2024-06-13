970f68954298b8de303a34a3975c3a7d6d5dcb49
Pierre GAMEN <pierre.gamen@gmail.com>
2024-06-13 11:15:09 +0200

    Add: link to winter Energie website on footer

--- src/app/components/Footer/footer.jsx
+++ src/app/components/Footer/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         src="https://assets-global.website-files.com/64e0b797e5ca48456ffda312/64e53e34eab4da497a5a78e1_Groupe%201083.svg" 
         alt="Flamme et décoration" />
         <div>
-        <p>Site original : Winter Energie</p>
+        <p>Site original : <a href="https://www.winter-energies.fr/">Winter Energie</a></p>
         <p>Réinterpreté par : Pierre GAMEN</p>
         <p>Site réalisé avec le FrameWork Next.js et déployé avec Vercel</p>
         </div>
